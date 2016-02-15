0010b3ac23729e741d99936e35e53c9d53e829b8
unknown <anthshor@gmail.com>
2016-02-15 15:33:16 +1300

    Fixed bug in dbca -createDatabase

--- create_db_11g.sh
+++ create_db_11g.sh
@@ -12,7 +12,8 @@ if [ $? -eq 0 ]; then
 	echo "INFO: Database ${ORACLE_SID} exists, skipping database creation."
 else
 	echo "INFO: Creating database ${ORACLE_SID}..."
-	sudo -E -H -u oracle /u01/app/oracle/product/${VERSION}/dbhome_1/bin/dbca -silent -createDatabase -templateName /vagrant/fred.dbc -gdbName $ORACLE_SID -sid $ORACLE_SID -sysPassword $PASSWORD \
-	-systemPassword $PASSWORD -emConfiguration LOCAL -sysmanPassword $PASSWORD -dbsnmpPassword $PASSWORD -storageType ASM -diskGroupName +DATA -recoveryGroupName +FRA \
-	-characterset WE8ISO8859P1 -obfuscatedPasswords false -sampleSchema false -asmSysPassword $ASMPWD
+	sudo -E -H -u oracle /u01/app/oracle/product/${VERSION}/dbhome_1/bin/dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbName ORCL -sid ORCL -SysPassword change_on_install -SystemPassword manager -emConfiguration NONE -storageType ASM -asmSysPassword change_on_install -diskGroupName DATA -recoveryGroupName FRA -characterSet WE8ISO8859P15 -memoryPercentage 40
+	#sudo -E -H -u oracle /u01/app/oracle/product/${VERSION}/dbhome_1/bin/dbca -silent -createDatabase -templateName /vagrant/fred.dbc -gdbName $ORACLE_SID -sid $ORACLE_SID -responseFile NO_VALUE -sysPassword $PASSWORD \
+	#-systemPassword $PASSWORD -emConfiguration LOCAL -sysmanPassword $PASSWORD -dbsnmpPassword $PASSWORD -storageType ASM -diskGroupName +DATA -recoveryGroupName +FRA \
+	#-characterset WE8ISO8859P1 -obfuscatedPasswords false -sampleSchema false -asmSysPassword $ASMPWD
 fi
